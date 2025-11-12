915a384fbfa28e09f45c939801a4444c69343ccc
Megh-patel-2008 <m3ghpatel2008@gmail.com>
2025-11-11 21:52:23 -0500

    Question 5

--- Review_part_2.java
+++ Review_part_2.java
@@ -41,13 +41,12 @@ public class Review_part_2
         }
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    public void printNumbers(){
+        int i = 1;
+        while(i <=10)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
 }
